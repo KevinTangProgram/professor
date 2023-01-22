ee60eb6d4ed50b10748981312e67155e3b0e485d
KevinTangProgram <121833798+KevinTangProgram@users.noreply.github.com>
2023-01-21 22:58:42 -0800

    Add files via upload

---
+++ precache-manifest.375374fc34404a815516f591efb66fa8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "36f4cd7f4d6646d29c17",
+    "url": "/professor/static/js/main.36f4cd7f.chunk.js"
+  },
+  {
+    "revision": "6fd8969915401c84bb67",
+    "url": "/professor/static/js/1.6fd89699.chunk.js"
+  },
+  {
+    "revision": "3e548f5e8c546a7be571",
+    "url": "/professor/static/js/runtime~main.3e548f5e.js"
+  },
+  {
+    "revision": "e5a2801da841b14e098bafb579aacb02",
+    "url": "/professor/index.html"
+  }
+];
